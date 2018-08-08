41192685ff9463d821117955b1e522923ead0290
utopian <wuthering1118@gmail.com>
2018-08-09 00:50:28 +0900

    add comments on mySum

--- CH1/mySum.hs
+++ CH1/mySum.hs
@@ -1,10 +1,13 @@
 
+-- This recursion builds up thunks.
 mySum ::(Num a) => [a] -> a
 mySum [] = 0
 mySum (a:as) = a + mySum as
 
+-- This is tail recursion, but finally builds up thunks.
 mySum2 s [] = s
 mySum2 s (x:xs) = let s' = s + x in mySum2 s' xs
 
+-- Prevent thunks by evaluating strictly.
 mySum2' s [] = s
-mySum2' s (x:xs) = let s' = s + x in s' `seq` mySum2' s' xs 
+mySum2' s (x:xs) = let s' = s + x in s' `seq` mySum2' s' xs
